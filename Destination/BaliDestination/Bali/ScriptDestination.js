eaa8c22c3f1ce3ee04c4a78a826740f723ad8fcf
maderafikana <102606176+maderafikana@users.noreply.github.com>
2026-01-08 18:00:28 +0700

    Delete Destination/BaliDestination/Bali directory

--- Destination/BaliDestination/Bali/ScriptDestination.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("navMenu");
-
-hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
